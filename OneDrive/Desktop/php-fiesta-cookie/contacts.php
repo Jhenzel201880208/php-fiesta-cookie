f42989991e9972e28f94ecefb33445237ac4009e
dianarod28 <201880207@psu.palawan.edu.ph>
2022-02-24 15:41:44 +0800

    inc directory done

--- OneDrive/Desktop/php-fiesta-cookie/contacts.php
+++ OneDrive/Desktop/php-fiesta-cookie/contacts.php
@@ -3,6 +3,7 @@
 <body>
 	<?php
 	echo "My name is Jhenzel Rodriguez and my contact number is 09925515221";
+	echo "My name is Diana Rodriguez and my contact number is 09111177777"
 ?>
 </body>
 </html>
